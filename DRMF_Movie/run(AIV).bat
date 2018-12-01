1bdbcea078bc98ad871ab1fccf2357d98bb67d14
sayhitosandy <sanidhya15085@iiitd.ac.in>
2018-12-02 01:57:07 +0530

    Cleaned

--- DRMF_Movie/run(AIV).bat
+++
@@ -1,7 +0,0 @@
-# run tests with fixed lambda_u and lambda_v
-
-python ./run.py -d ./test/AIV/1 -a ./test/AIV/1 -o ./test/AIV/1 -e 200 -p ./data/glove/glove.6B.200d.txt -g False
-#python ./run.py -d ./test/AIV/2 -a ./test/AIV/2 -o ./test/AIV/2 -e 200 -p ./data/glove/glove.6B.200d.txt -g False
-#python ./run.py -d ./test/AIV/3 -a ./test/AIV/3 -o ./test/AIV/3 -e 200 -p ./data/glove/glove.6B.200d.txt -g False
-#python ./run.py -d ./test/AIV/4 -a ./test/AIV/4 -o ./test/AIV/4 -e 200 -p ./data/glove/glove.6B.200d.txt -g False
-#python ./run.py -d ./test/AIV/5 -a ./test/AIV/5 -o ./test/AIV/5 -e 200 -p ./data/glove/glove.6B.200d.txt -g False
